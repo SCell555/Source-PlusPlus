1df788b4da880657a3136395e2c786480262a093
Joshua Ashton <jashton@punyhuman.com>
2017-12-04 01:49:42 +0000

    Remove trailing backslash from run_mod.bat's -games

--- game/hl2/run_mod.bat
+++ game/hl2/run_mod.bat
@@ -1,1 +1,5 @@
-"C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Multiplayer\hl2.exe" -game %~dp0
+@echo off
+
+set GameDir=%~dp0
+set GameDir=%GameDir:~0,-1%
+"C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Multiplayer\hl2.exe" -game %GameDir%
