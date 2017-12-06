ffe70c829728160c4ce8f789eceb419bdb5308bb
Joshua Ashton <jashton@punyhuman.com>
2017-12-06 18:40:41 +0000

    Add other SP games from SDK 2007

--- src/createallprojects.bat
+++ src/createallprojects.bat
@@ -1,1 +1,1 @@
-devtools\bin\vpc.exe /2013 /hl2mp /hl2 /episodic +everything /mksln everything.sln
+devtools\bin\vpc.exe /2013 /hl2mp /hl2 /episodic /hl1 /cstrike /portal /lostcoast /dod +everything /mksln everything.sln
